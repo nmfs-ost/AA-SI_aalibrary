a8e83d4f694b76f6dba7afe94b880f06ca1a6400
Hannan.Khan <hannan.khan@noaa.gov>
2024-10-04 17:10:17 -0500

    added code for uploading files to gcp

--- ingestion.py
+++ ingestion.py
@@ -8,10 +8,12 @@ import subprocess
 import requests
 import time
 from typing import List
+import logging
 
 import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
+from google.cloud import bigquery, storage
 
 import utils
 
@@ -35,6 +37,22 @@ def get_file_paths_via_json_link(link: str = ""):
         cloud_path = i['attributes']['CLOUD_PATH']
         if cloud_path:
             print(f"{file_name}, {cloud_path}")
+
+
+def parse_variables_from_ncei_file_url(url: str = ""):
+    """Gets the file variables associated with a file url in NCEI.
+    File urls in NCEI follow this template:
+    data/raw/{ship_name}/{survey_name}/{echosounder}/{file_name}
+    
+    NOTE: file_name will include the extension."""
+
+    file_name = get_file_name_from_url(url=url)
+    file_type = file_name.split(".")[-1]
+    echosounder = url.split("/")[-2]
+    survey_name = url.split("/")[-3]
+    ship_name = url.split("/")[-4]
+
+    return file_name, file_type, echosounder, survey_name, ship_name
 
 
 def get_file_name_from_url(url: str = ""):
@@ -94,6 +112,7 @@ def download_single_survey_from_ncei(ship_name: str = "",
         download_location (str, optional): _description_. Defaults to "".
     """    
     # Get a list of urls of objects from that folder.
+    # TODO: Check if its already cached.
     # TODO
     ...
 
@@ -154,6 +173,85 @@ def get_all_objects_in_survey_from_ncei(ship_name: str = "",
         survey_objects.append(object.key)
     
     return survey_objects
+
+
+def parse_correct_gcp_storage_bucket_location(file_name: str = "",
+                                              file_type: str = "",
+                                              ship_name: str = "",
+                                              survey_name: str = "",
+                                              echosounder: str = "",
+                                              data_source: str = "NCEI",
+                                              is_metadata: bool = False,
+                                              debug: bool = False):
+    """Calculates the correct gcp storage location based on data source, file
+    type, and if the file is metadata or not.
+
+    Args:
+        file_name (str, optional): The file name (includes extension). Defaults to "".
+        file_type (str, optional): The file type (not include the dot "."). Defaults to "".
+        ship_name (str, optional): The ship name associated with this survey. Defaults to "".
+        survey_name (str, optional): The survey name/identifier. Defaults to "".
+        echosounder (str, optional): The echosounder used to gather the data. Defaults to "".
+        data_source (str, optional): The source of the data. Can be one of ["NCEI", "OMAO"]. Defaults to "NCEI".
+        is_metadata (bool, optional): Whether or not the file is a metadata file. Necessary since
+            files that are considered metadata (metadata json, or readmes) are stored
+            in a separate directory. Defaults to False.
+        debug (bool, optional): Whether or not to print debug statements. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    # Creating the correct upload location
+    if not is_metadata:
+        # Figure out if its a raw or idx file (belongs in raw folder)
+        if file_type.lower() in ["raw", "idx"]:
+            gcp_storage_bucket_location = f"{data_source}/{ship_name}/{survey_name}/{echosounder}/data/raw/{file_name}"
+        elif file_type.lower() in ["netCDF"]:
+            gcp_storage_bucket_location = f"{data_source}/{ship_name}/{survey_name}/{echosounder}/data/netcdf/{file_name}"
+    else:
+        gcp_storage_bucket_location = f"{data_source}/{ship_name}/{survey_name}/{echosounder}/metadata/"
+    
+    if debug:
+        print(f"PARSED GCP_STORAGE_BUCKET_LOCATION: {gcp_storage_bucket_location}")
+
+    return gcp_storage_bucket_location
+
+
+def upload_file_to_gcp_storage_bucket(file_name: str = "",
+                                      file_type: str = "",
+                                      ship_name: str = "",
+                                      survey_name: str = "",
+                                      echosounder: str = "",
+                                      file_location: str = "",
+                                      gcp_bucket: storage.Client.bucket = None,
+                                      data_source: str = "NCEI",
+                                      is_metadata: bool = False,
+                                      debug: bool = False):
+    """Uploads a local file to the storage bucket.
+
+    Args:
+        file_name (str, optional): The file name (includes extension). Defaults to "".
+        file_type (str, optional): The file type (not include the dot "."). Defaults to "".
+        ship_name (str, optional): The ship name associated with this survey. Defaults to "".
+        survey_name (str, optional): The survey name/identifier. Defaults to "".
+        echosounder (str, optional): The echosounder used to gather the data. Defaults to "".
+        file_location (str, optional): The local location of the file. Defaults to "".
+        gcp_bucket (storage.Client.bucket, optional): The GCP bucket object used to upload
+            the file. Defaults to None.
+        data_source (str, optional): The source of the data. Can be one of ["NCEI", "OMAO"]. Defaults to "NCEI".
+        is_metadata (bool, optional): Whether or not the file is a metadata file. Necessary since
+            files that are considered metadata (metadata json, or readmes) are stored
+            in a separate directory. Defaults to False.
+        debug (bool, optional): Whether or not to print debug statements. Defaults to False.
+    """
+    
+    gcp_storage_bucket_location = parse_correct_gcp_storage_bucket_location()
+
+    # Upload to storage bucket.
+    utils.upload_file_to_gcp_bucket(bucket=gcp_bucket, blob_file_path=gcp_storage_bucket_location,
+                                    local_file_path=file_location, debug=debug)
+
+    return
 
 
 if __name__ == '__main__':
