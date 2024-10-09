ce5edb995c0cc9dcc4bc637081397522323bed08
Hannan.Khan <hannan.khan@noaa.gov>
2024-10-09 17:16:13 -0500

    more code improvements

--- ingestion.py
+++ ingestion.py
@@ -86,13 +86,13 @@ def create_ncei_url_from_variables(file_name: str = "",
         # TODO: make sure to check that a raw and idx files both exist.
 
 
-def download_single_file_from_aws(s3_bucket_name: str = "noaa-wcsd-pds",
+def download_single_file_from_aws(s3_bucket: str = "noaa-wcsd-pds",
                                   file_url: str = "https://noaa-wcsd-pds.s3.amazonaws.com/data/raw/Reuben_Lasker/RL2107/EK80/2107RL_CW-D20210706-T172335.idx",
                                   download_location: str = ""):
     """Downloads a file from AWS storage bucket, aka the NCEI repository."""
     
     try:
-        s3_client, s3_resource, s3_bucket_name = utils.create_s3_objs()
+        s3_client, s3_resource, s3_bucket = utils.create_s3_objs()
     except Exception as e:
         print(f"Cannot establish connection to s3 bucket..\n{e}")
     
@@ -101,12 +101,19 @@ def download_single_file_from_aws(s3_bucket_name: str = "noaa-wcsd-pds",
     file_url = file_url.replace("s3://noaa-wcsd-pds/", '')
     file_name = get_file_name_from_url(file_url)
 
+    # Check if the file exists in s3
+    print(f"s3_bucket.name: {s3_bucket.name}")
+    file_exists = utils.check_if_file_exists_in_s3(object_key=file_url,
+                                                   s3_resource=s3_resource,
+                                                   s3_bucket_name=s3_bucket.name)
+
     # Finally download the file.
     try:
-        s3_bucket_name.download_file(file_url, file_name)
+        s3_bucket.download_file(file_url, file_name, download_location)
         print(f"Downloaded: {file_name} to {download_location}")
     except Exception as e:
         print(f"Error downloading file {file_name}.\n{e}")
+        return
 
 
 def download_single_survey_from_ncei(ship_name: str = "",
@@ -204,6 +211,7 @@ def download_raw_file(file_name: str = "",
     """
 
     # Create vars for use later.
+    file_download_location = os.sep.join([os.path.normpath(file_download_location), file_name])
     file_ncei_url = create_ncei_url_from_variables(file_name=file_name, ship_name=ship_name,
                                                    survey_name=survey_name, echosounder=echosounder)
     file_name_idx = ".".join(file_name.split(".")[:-1]) + ".idx"
@@ -232,7 +240,7 @@ def download_raw_file(file_name: str = "",
             try:
                 print("FORCE DOWNLOAD FROM NCEI WAS ENABLED")
                 print(f"DOWNLOADING FILE {file_name} FROM NCEI")
-                download_single_file_from_aws(s3_bucket_name="noaa-wcsd-pds",
+                download_single_file_from_aws(s3_bucket="noaa-wcsd-pds",
                                             file_url=file_ncei_url,
                                             download_location=file_download_location)
                 print(f"DOWNLOADED FILE {file_name} FROM NCEI")
@@ -242,7 +250,7 @@ def download_raw_file(file_name: str = "",
             # Force download and upload the idx file.
             try:
                 print(f"DOWNLOADING IDX FILE {file_name_idx} FROM NCEI")
-                download_single_file_from_aws(s3_bucket_name="noaa-wcsd-pds",
+                download_single_file_from_aws(s3_bucket="noaa-wcsd-pds",
                                             file_url=file_ncei_idx_url,
                                             download_location=file_download_location_idx)
                 print(f"DOWNLOADED FILE {file_name_idx} FROM NCEI\nUPLOADING TO GCP...")
@@ -252,7 +260,7 @@ def download_raw_file(file_name: str = "",
         else:
             # Here we download the raw file from GCP. We also check for a netcdf
             # version and let the user know.
-            print(f"SINCE FILE EXISTS IN GCP, CHECKING FOR NETCDF VERSION...")
+            print(f"CHECKING FOR NETCDF VERSION...")
             netcdf_exists_in_gcp = check_if_netcdf_file_exists_in_gcp(file_name=file_name_netcdf,
                                                                       file_type="netcdf",
                                                                       ship_name=ship_name,
@@ -267,9 +275,10 @@ def download_raw_file(file_name: str = "",
                 print(f"FILE {file_name} EXISTS AS A NETCDF ALREADY. PLEASE DOWNLOAD THE NETCDF VERSION IF NEEDED.")
             else:
                 print(f"FILE `{file_name}` DOES NOT EXIST AS NETCDF. CONSIDER RUNNING A CONVERSION FUNCTION")
+            
+            # Here we download the raw from GCP.
             try:
-                # Here we download the raw from GCP.
-                print(f"DOWNLOADING FILE `{file_name}` TO `{file_download_location}`")
+                print(f"DOWNLOADING FILE `{file_name}` FROM GCP TO `{file_download_location}`")
                 utils.download_file_from_gcp(gcp_bucket=gcp_bucket, blob_file_path=gcp_storage_bucket_location,
                                             local_file_path=file_download_location, debug=debug)
                 print(f"DOWNLOADED.")
@@ -280,8 +289,8 @@ def download_raw_file(file_name: str = "",
     else:
         # Download and upload the raw file.
         try:
-            print(f"DOWNLOADING FILE {file_name} FROM NCEI")
-            download_single_file_from_aws(s3_bucket_name="noaa-wcsd-pds",
+            print(f"DOWNLOADING FILE `{file_name}` FROM NCEI...")
+            download_single_file_from_aws(s3_bucket="noaa-wcsd-pds",
                                         file_url=file_ncei_url,
                                         download_location=file_download_location)
             print(f"DOWNLOADED FILE {file_name} FROM NCEI\nUPLOADING TO GCP...")
@@ -306,7 +315,7 @@ def download_raw_file(file_name: str = "",
         # Download and upload the idx file.
         try:
             print(f"DOWNLOADING IDX FILE {file_name_idx} FROM NCEI")
-            download_single_file_from_aws(s3_bucket_name="noaa-wcsd-pds",
+            download_single_file_from_aws(s3_bucket="noaa-wcsd-pds",
                                         file_url=file_ncei_idx_url,
                                         download_location=file_download_location_idx)
             print(f"DOWNLOADED FILE {file_name_idx} FROM NCEI\nUPLOADING TO GCP...")
@@ -523,7 +532,7 @@ if __name__ == '__main__':
     # print(utils.count_objects_in_bucket_location(prefix="data/raw/Reuben_Lasker/RL2107/",
     #                                              bucket=bucket))
     
-    file_name, file_type, echosounder, survey_name, ship_name = parse_variables_from_ncei_file_url(url="https://noaa-wcsd-pds.s3.amazonaws.com/data/raw/Reuben_Lasker/RL2107/EK80/2107RL_CW-D20210813-T220732.raw")
+    # file_name, file_type, echosounder, survey_name, ship_name = parse_variables_from_ncei_file_url(url="https://noaa-wcsd-pds.s3.amazonaws.com/data/raw/Reuben_Lasker/RL2107/EK80/2107RL_CW-D20210813-T220732.raw")
     # print(parse_correct_gcp_storage_bucket_location(file_name=file_name,
     #                                                 file_type=file_type,
     #                                                 ship_name=ship_name,
@@ -533,12 +542,15 @@ if __name__ == '__main__':
     #                                                 is_metadata=False,
     #                                                 debug=True))
     # https://noaa-wcsd-pds.s3.amazonaws.com/data/raw/Reuben_Lasker/RL2107/EK80/2107RL_CW-D20210706-T172335.idx
+    # print(utils.check_if_file_exists_in_s3(object_key="data/raw/Reuben_Lasker/RL2107/EK80/2107RL_CW-D20210706-T172335.idx",
+    #                                  s3_resource=s3_resource,
+    #                                  s3_bucket_name="noaa-wcsd-pds"))
     download_raw_file(file_name="2107RL_CW-D20210813-T220732.raw",
                                 file_type="raw",
                                 ship_name="Reuben_Lasker",
                                 survey_name="RL2107",
                                 echosounder="EK80",
-                                file_download_location=f"./2107RL_CW-D20210813-T220732.raw",
+                                file_download_location=f"./",
                                 is_metadata=False,
                                 force_download_from_ncei=False,
                                 debug=True)
