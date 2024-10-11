32f5dce53667068062761b5190cd08425c847f04
Hannan.Khan <hannan.khan@noaa.gov>
2024-10-11 15:34:38 -0500

    moved some constant vars over to config

--- app/config.py
+++ app/config.py
@@ -1,2 +1,5 @@
 # Used for storing environment-specific settings such as database URIs and such.
 
+RAW_DATA_FILE_TYPES = ["raw", "idx"]
+CONVERTED_DATA_FILE_TYPES = ["netcdf"]
+METADATA_FILE_TYPES = ["json"]
