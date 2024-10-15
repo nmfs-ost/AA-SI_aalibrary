6fe3c715a69751e1a186ebd7ae736ae84f5c6e29
Hannan.Khan <hannan.khan@noaa.gov>
2024-10-15 00:23:05 -0500

    Added code for routing to the correct function

---
+++ app/routes.py
@@ -0,0 +1,59 @@
+"""This app routes the request to the correct function."""
+
+from flask import request
+
+from app import ingestion
+from app import api_app
+
+
+@api_app.route("/")
+def index():
+    return "Welcome to the Active Acoustics API"
+
+
+@api_app.route("/hello/")
+def hello_world():
+    return "Hello World"
+
+
+@api_app.route("/download-raw/")
+def download_raw():
+    file_name = request.args.get("file_name")
+    file_type = request.args.get("file_type")
+    ship_name = request.args.get("ship_name")
+    survey_name = request.args.get("survey_name")
+    echosounder = request.args.get("echosounder")
+    force_download_from_ncei = request.args.get("force_download_from_ncei")
+    debug = request.args.get("debug")
+
+    ingestion.download_raw_file(file_name=file_name,
+                                file_type=file_type,
+                                ship_name=ship_name,
+                                survey_name=survey_name,
+                                echosounder=echosounder,
+                                file_download_location=".",
+                                is_metadata=False,
+                                force_download_from_ncei=force_download_from_ncei,
+                                debug=debug)
+
+
+@api_app.route("/download_netcdf/")
+def download_netcdf():
+    file_name = request.args.get("file_name")
+    file_type = request.args.get("file_type")
+    ship_name = request.args.get("ship_name")
+    survey_name = request.args.get("survey_name")
+    echosounder = request.args.get("echosounder")
+    data_source = request.args.get("data_source")
+    debug = request.args.get("debug")
+
+    ingestion.download_netcdf_file(file_name=file_name,
+                                   file_type=file_type,
+                                   ship_name=ship_name,
+                                   survey_name=survey_name,
+                                   echosounder=echosounder,
+                                   data_source=data_source,
+                                   file_download_location=".",
+                                   gcp_bucket=gcp_bucket,
+                                   is_metadata=False,
+                                   debug=debug)
