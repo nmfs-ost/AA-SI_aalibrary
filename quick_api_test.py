8d51e294cb25728be05cef5b75ec0520b80dd7ee
Hannan.Khan <hannan.khan@noaa.gov>
2024-10-28 12:08:58 -0500

    commit: finished up quick api testing script

--- quick_api_test.py
+++ quick_api_test.py
@@ -2,6 +2,29 @@
 Also checks to see if a raw file can be downloaded."""
 
 from app.utils import cloud_utils
+from app import ingestion
 
 _, _, _ = cloud_utils.setup_gcp_storage_objs()
 
+# download a raw file
+file_name = "2107RL_CW-D20210813-T220732.raw"
+file_name_idx = "2107RL_CW-D20210813-T220732.idx"
+file_type = "raw"
+ship_name = "Reuben_Lasker"
+survey_name = "RL2107"
+echosounder = "EK80"
+data_source = "TEST"
+file_download_location = "."
+is_metadata = False
+ingestion.download_raw_file(
+    file_name=file_name,
+    file_type=file_type,
+    ship_name=ship_name,
+    survey_name=survey_name,
+    echosounder=echosounder,
+    data_source=data_source,
+    file_download_location=file_download_location,
+    is_metadata=False,
+    force_download_from_ncei=False,
+    debug=True,
+)
