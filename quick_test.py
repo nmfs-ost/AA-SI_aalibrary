91951ea7355df3c430e653667fb140b3c58b592b
Hannan.Khan <hannan.khan@noaa.gov>
2024-11-21 15:04:55 -0600

    change: more descriptive error checking

--- quick_test.py
+++ quick_test.py
@@ -1,12 +1,27 @@
 """Contains quick tests for the API to verify that the connections are working as intended.
 Also checks to see if a raw file can be downloaded."""
 
+import sys
+
 from src.aalibrary.utils import cloud_utils
 from src.aalibrary import ingestion
 
+
 # CONNECTION TEST: set up storage objects
-_, _, gcp_bucket = cloud_utils.setup_gcp_storage_objs()
-s3_client, s3_resource, s3_bucket = cloud_utils.create_s3_objs()
+try:
+    _, _, gcp_bucket = cloud_utils.setup_gcp_storage_objs()
+except Exception as e:
+    print(
+        f"CONNECTION TEST TO GCP FAILED DUE TO THE FOLLOWING ERROR:\n{e}",
+        file=sys.stderr,
+    )
+try:
+    s3_client, s3_resource, s3_bucket = cloud_utils.create_s3_objs()
+except Exception as e:
+    print(
+        f"CONNECTION TEST TO s3 FAILED DUE TO THE FOLLOWING ERROR:\n{e}",
+        file=sys.stderr,
+    )
 
 # FUNCTIONAL TEST: download a raw file
 file_name = "2107RL_CW-D20210813-T220732.raw"
