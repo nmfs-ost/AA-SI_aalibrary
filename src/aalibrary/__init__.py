784b59916c2c9e17cea1be2f46c93c084d4da7d4
Hannan.Khan <hannan.khan@noaa.gov>
2024-11-21 21:11:31 -0600

    chore: testing out different versions of logging, none seem to be working.

--- src/aalibrary/__init__.py
+++ src/aalibrary/__init__.py
@@ -1,8 +1,9 @@
 # set the correct logging here
+import sys
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
+    handlers=[logging.StreamHandler(sys.stdout)],
 )
