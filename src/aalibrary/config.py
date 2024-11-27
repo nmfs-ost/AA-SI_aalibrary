012d6db4a4ba05996298321f2352b1c9ea3d99f2
Hannan.Khan <hannan.khan@noaa.gov>
2024-11-27 16:39:12 -0600

    add: echosounders EK60 and EK500 as valid echosounders.

--- src/aalibrary/config.py
+++ src/aalibrary/config.py
@@ -5,5 +5,5 @@ CONVERTED_DATA_FILE_TYPES = ["netcdf", "nc"]
 METADATA_FILE_TYPES = ["json"]
 VALID_FILETYPES = ["raw", "idx", "netcdf", "nc", "json"]
 
-VALID_ECHOSOUNDERS = ["EK70", "EK80", "ES60"]
+VALID_ECHOSOUNDERS = ["EK70", "EK80", "ES60", "EK60", "EK500"]
 VALID_DATA_SOURCES = ["NCEI", "OMAO", "HDD", "TEST"]
