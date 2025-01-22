86e6853e33ce459b2e5722ca9589669f493a8ee5
Hannan.Khan <hannan.khan@noaa.gov>
2025-01-21 18:22:25 -0600

    add: .bot to raw and valid filetypes, and EK_60 to valid echosounders.

--- src/aalibrary/config.py
+++ src/aalibrary/config.py
@@ -1,9 +1,9 @@
 # Used for storing environment-specific settings such as database URIs and such.
 
-RAW_DATA_FILE_TYPES = ["raw", "idx"]
+RAW_DATA_FILE_TYPES = ["raw", "idx", "bot"]
 CONVERTED_DATA_FILE_TYPES = ["netcdf", "nc"]
 METADATA_FILE_TYPES = ["json"]
-VALID_FILETYPES = ["raw", "idx", "netcdf", "nc", "json"]
+VALID_FILETYPES = ["raw", "idx", "netcdf", "nc", "json", "bot"]
 
-VALID_ECHOSOUNDERS = ["EK70", "EK80", "ES60", "EK60", "EK500"]
+VALID_ECHOSOUNDERS = ["EK70", "EK80", "ES60", "EK60", "EK500", "EK_60"]
 VALID_DATA_SOURCES = ["NCEI", "OMAO", "HDD", "TEST"]
