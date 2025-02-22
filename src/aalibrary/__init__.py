7fce7e0373a0aec4c3be88d6cf9e8347ebf1e70b
Hannan.Khan <hannan.khan@noaa.gov>
2025-02-21 18:52:15 -0600

    fix: linting __init__.py

--- src/aalibrary/__init__.py
+++ src/aalibrary/__init__.py
@@ -2,11 +2,11 @@
 import sys
 import logging
 
+# Import sub-packages
+from . import utils
+
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
-
-# Import sub-packages
-from . import utils
