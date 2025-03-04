0d32d7ca65b7cf9609adca41bd9be325a93e8a63
Hannan.Khan <hannan.khan@noaa.gov>
2025-03-03 19:34:14 -0600

    add: console level debug handler to logging

--- src/aalibrary/__init__.py
+++ src/aalibrary/__init__.py
@@ -11,3 +11,12 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
+
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger("").addHandler(console)
