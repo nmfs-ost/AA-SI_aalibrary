d9d9a93d1699699fc250b3f50d967a1961354633
Michael C Ryan <spacetime.engineer@gmail.com>
2025-03-31 17:22:27 -0400

    Updated Dockerfile
    Updated Dockerfile

--- docker/gcloud_login.sh
+++ docker/gcloud_login.sh
@@ -1,18 +1,40 @@
-
 #!/bin/bash
 
+# Ensure required environment variables are set
+if [ -z "$ACCOUNT" ] || [ -z "$PROJECT" ]; then
+    echo "Error: ACCOUNT and PROJECT environment variables must be set."
+    exit 1
+fi
+
+# Authenticate using the user account
+echo "Authenticating user..."
+gcloud auth login --no-browser --quiet
+if [ $? -ne 0 ]; then
+    echo "Failed to authenticate user."
+    exit 1
+fi
 
 # Set the account from the environment variable ACCOUNT
-gcloud config set account ${ACCOUNT}
-echo "Account set to ${ACCOUNT}"
+echo "Setting active account to: $ACCOUNT"
+gcloud config set account "$ACCOUNT"
+
 # Set the project from the environment variable PROJECT
-gcloud config set project ${PROJECT}
-echo "Project set to ${PROJECT}"
-# Authenticate using the user account
-gcloud auth login --no-browser
+echo "Setting project to: $PROJECT"
+gcloud config set project "$PROJECT"
 
 # Authenticate using application default credentials
-gcloud auth application-default login --no-browser
+echo "Authenticating application default credentials..."
+gcloud auth application-default login --no-browser --quiet
+if [ $? -ne 0 ]; then
+    echo "Failed to authenticate application default credentials."
+    exit 1
+fi
 
-# Start the main process
-exec "$@"
+# Start the main process if arguments are provided
+if [ $# -gt 0 ]; then
+    echo "Executing: $@"
+    exec "$@"
+else
+    echo "No command provided, exiting."
+    exit 0
+fi
