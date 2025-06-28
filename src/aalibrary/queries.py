a94aa51736a4f2ec1e2ffbe39c8fcb9092a47f79
Hannan.Khan <hannan.khan@noaa.gov>
2025-06-27 23:28:31 -0500

    add: TODOs and possible functions

--- src/aalibrary/queries.py
+++ src/aalibrary/queries.py
@@ -7,9 +7,27 @@ from dataclasses import dataclass
 
 
 @dataclass
-class MetadataQueries():
+class MetadataQueries:
     """This class contains queries related to the upload, alteration, and
     retrieval of metadata from our BigQuery instance.
     """
+
     get_all_aalibrary_metadata_records: str = """
     SELECT * FROM `ggn-nmfs-aa-dev-1.metadata.aalibrary_file_metadata`"""
+
+    # TODO for mike ryan
+    get_all_possible_ship_names_from_database: str = """
+    SELECT ship_name from `ggn-nmfs-aa-dev-1.metadata.aalibrary_file_metadata`
+    """
+
+    def get_all_surveys_associated_with_a_ship_name(ship_name: str = ""):
+        get_all_surveys_associated_with_a_ship_name_query: str = """"""
+        return get_all_surveys_associated_with_a_ship_name_query
+
+    def get_all_echosounders_used_in_a_survey(survey: str = ""): ...
+
+    def get_all_netcdf_files_in_database(): ...
+
+
+if __name__ == "__main__":
+    ...
