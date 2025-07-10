04aaee9c90380b6457d9cac6b77ef5714a761755
Brett L <leasure57@gmail.com>
2025-07-10 10:06:34 -0600

    working on streaming a file

--- src/aalibrary/ingestion.py
+++ src/aalibrary/ingestion.py
@@ -1663,6 +1663,96 @@ def find_data_source_for_file():
     ...
 
 
+def stream_raw_file_from_ncei(
+    file_name: str = "",
+    file_type: str = "raw",
+    ship_name: str = "",
+    survey_name: str = "",
+    echosounder: str = "",
+    data_source: str = "NCEI",
+    is_metadata: bool = False,
+    debug: bool = False,
+):
+    """Similar to download_raw_file_from_ncei but instead of downloading the file,
+    it returns a file object that can be streamed to the client.
+    
+    This function is designed for API use to enable streaming files directly to clients.
+
+    Args:
+        file_name (str, optional): The file name (includes extension).
+            Defaults to "".
+        file_type (str, optional): The file type (do not include the dot ".").
+            Defaults to "".
+        ship_name (str, optional): The ship name associated with this survey.
+            Defaults to "".
+        survey_name (str, optional): The survey name/identifier.
+            Defaults to "".
+        echosounder (str, optional): The echosounder used to gather the data.
+            Defaults to "".
+        data_source (str, optional): The source of the file. Necessary due to
+            the way the storage bucket is organized. Can be one of
+            ["NCEI", "OMAO", "HDD"]. Defaults to "".
+        is_metadata (bool, optional): Whether or not the file is a metadata
+            file. Necessary since files that are considered metadata (metadata
+            json, or readmes) are stored in a separate directory. Defaults to
+            False.
+        debug (bool, optional): Whether or not to print debug statements.
+            Defaults to False.
+            
+    Returns:
+        tuple: A tuple containing:
+            - stream (io.BytesIO): A BytesIO object containing the file data to be streamed
+            - content_type (str): The content type of the file
+            - filename (str): The name of the file for use in Content-Disposition headers
+    """
+    import io
+    
+    try:
+        s3_client, s3_resource, s3_bucket = utils.cloud_utils.create_s3_objs()
+    except Exception as e:
+        logging.error(f"CANNOT ESTABLISH CONNECTION TO S3 BUCKET..\n{e}")
+        raise
+
+    rf = RawFile(
+        file_name=file_name,
+        file_type=file_type,
+        ship_name=ship_name,
+        survey_name=survey_name,
+        echosounder=echosounder,
+        data_source=data_source,
+        # We don't need a download directory as we're streaming
+        file_download_directory="",
+        is_metadata=is_metadata,
+        upload_to_gcp=False,
+        debug=debug,
+        s3_resource=s3_resource,
+    )
+
+    # Create a BytesIO object to stream the file
+    file_stream = io.BytesIO()
+    content_type = "application/octet-stream"
+    
+    if rf.raw_file_exists_in_ncei:
+        # Get the file object from S3
+        s3_object = s3_resource.Object("noaa-wcsd-pds", rf.raw_file_ncei_url)
+        # Download the file to the BytesIO object
+        s3_object.download_fileobj(file_stream)
+        # Reset the file pointer to the beginning
+        file_stream.seek(0)
+        
+        # Determine content type based on file extension
+        if file_name.lower().endswith('.raw'):
+            content_type = "application/octet-stream"
+        elif file_name.lower().endswith('.idx'):
+            content_type = "application/octet-stream"
+        elif file_name.lower().endswith('.bot'):
+            content_type = "application/octet-stream"
+        
+        return file_stream, content_type, file_name
+    else:
+        raise FileNotFoundError(f"File {file_name} does not exist in NCEI storage")
+
+
 if __name__ == "__main__":
     # set logging config
     for handler in logging.root.handlers[:]:
