2156dd4a464898ce03769ac84e1b844ec0b37d6a
Hannan Khan <hannan.khan@noaa.gov>
2025-07-30 20:13:57 +0000

    add: init script for interacting with Tugboat API.

---
+++ src/aalibrary/tugboat_api.py
@@ -0,0 +1,49 @@
+"""This file contains functions to interface with the Tugboat API."""
+
+import sys
+import os
+import json
+
+# For pytests-sake
+if __package__ is None or __package__ == "":
+    # uses current directory visibility
+    from utils import cloud_utils
+else:
+    # uses current package visibility
+    from aalibrary.utils import cloud_utils
+
+
+class TugboatAPI:
+    """
+    This class provides methods to interact with the Tugboat API.
+    """
+
+    def __init__(self):
+        """
+        Initializes the TugboatAPI class.
+        """
+        self.__dict__.update(kwargs)
+        _create_vars()
+        _get_tugboat_credentials()
+        pass
+
+    def _create_vars(self):
+        """Creates vars for use later."""
+
+        # Create google connection objects to download the Tugboat creds
+        if (
+            ("gcp_stor_client" not in self.__dict__)
+            or ("gcp_bucket_name" not in self.__dict__)
+            or ("gcp_bucket" not in self.__dict__)
+        ):
+            self.gcp_stor_client, self.gcp_bucket_name, self.gcp_bucket = cloud_utils.setup_gcp_storage_objs()
+
+
+    def _get_tugboat_credentials(self):
+        """Gets the tugboat credentials for AALibrary from the google storage bucket."""
+        local_creds_file_path = r"~/tugboat_credentials.json"
+        cred_file = cloud_utils.download_file_from_gcp(gcp_bucket=self.gcp_bucket, blob_file_path="ggn-nmfs-aa-dev-1-data/other/tugboat_credentials.json",local_file_path=local_creds_file_path,debug=False)
+        with open(local_creds_file_path, 'r') as f:
+            creds_file = json.load(f)
+        self.tugboat_cred = creds_file["CREDENTIALS"]
+    
