b72508b2065dc8e09cf0c5163d869a4314349774
Michael C Ryan <michael.c.ryan@noaa.gov>
2025-07-31 11:54:54 -0400

    Update aa_find.py

--- src/aalibrary/console/aa_find.py
+++ src/aalibrary/console/aa_find.py
@@ -4,19 +4,20 @@ from aalibrary.utils.ncei_utils import (
     get_all_echosounders_in_a_survey,
     get_all_ship_names_in_ncei,
     get_all_file_names_from_survey,
-    get_all_raw_file_names_from_survey
+    get_all_raw_file_names_from_survey,
 )
 from InquirerPy import inquirer
 import subprocess, os
 from loguru import logger
 
 
-
 def main():
 
     mode = inquirer.select(
         message="Select option",
-        choices=["Search By NCEI Vessels", ],
+        choices=[
+            "Search By NCEI Vessels",
+        ],
         default="Search By NCEI Vessels",
     ).execute()
 
@@ -35,9 +36,9 @@ def main():
             message="Select Echosounder from " + survey,
             choices=get_all_echosounders_in_a_survey(ship_name, survey),
         ).execute()
-        
+
         # Get all file names for the selected survey
-        
+
         file_name = inquirer.select(
             message="Select Files from " + survey,
             choices=get_all_raw_file_names_from_survey(ship_name, survey, echosounder),
@@ -48,32 +49,41 @@ def main():
             choices=["Download .raw", "Download .nc ", "Plot Echogram", "Run KMeans"],
         ).execute()
 
-
-
-
         if operation == "Download .raw":
             # Define the folder name
             folder_name = ship_name + "_" + survey + "_" + echosounder + "_" + "NCEI"
 
             # Create the full path using '.'
-            path = os.path.join('.', folder_name)
+            path = os.path.join(".", folder_name)
 
             # Make the directory (does nothing if it already exists)
             os.makedirs(path, exist_ok=True)
-            logger.info(f"Downloading {echosounder} data for {ship_name} in {survey} to {folder_name}")
-            logger.debug(f"Running command: aa-raw --file_name {folder_name} --ship_name {ship_name} --survey_name {survey} --echosounder {echosounder} --file_download_directory {folder_name} from directory: {os.getcwd()} in python environment: {os.environ.get('VIRTUAL_ENV', 'No virtual environment')}")
-            subprocess.run([
-                "aa-raw",
-                "--file_name", file_name,
-                "--file_type", "raw",
-                "--ship_name", ship_name,
-                "--survey_name", survey,
-                "--echosounder", echosounder,
-                "--data_source", "NCEI",
-                "--file_download_directory", folder_name,
-            ])
-            
-    
+            logger.info(
+                f"Downloading {echosounder} data for {ship_name} in {survey} to {folder_name}"
+            )
+            logger.debug(
+                f"Running command: aa-raw --file_name {folder_name} --ship_name {ship_name} --survey_name {survey} --echosounder {echosounder} --file_download_directory {folder_name} from directory: {os.getcwd()} in python environment: {os.environ.get('VIRTUAL_ENV', 'No virtual environment')}"
+            )
+            subprocess.run(
+                [
+                    "aa-raw",
+                    "--file_name",
+                    file_name,
+                    "--file_type",
+                    "raw",
+                    "--ship_name",
+                    ship_name,
+                    "--survey_name",
+                    survey,
+                    "--echosounder",
+                    echosounder,
+                    "--data_source",
+                    "NCEI",
+                    "--file_download_directory",
+                    folder_name,
+                ]
+            )
+
 
 if __name__ == "__main__":
     main()
