46d4e0d80a0a01a150523490b26b5c8bf10c7b98
Hannan Khan <hannan.khan@tspi.net>
2026-01-07 12:34:09 -0600

    aasi-182 add: validations to tugboat

--- src/aalibrary/utils/tugboat_validations.py
+++ src/aalibrary/utils/tugboat_validations.py
@@ -1,6 +1,8 @@
 """This file contains validation functions for Tugboat submission JSON
 files."""
 
+# pylint: disable=missing-function-docstring,
+
 import sys
 import os
 import json
@@ -18,6 +20,27 @@ class TugboatValidator:
         self.submission_dict = submission_dict
         self._handle_submission_file_loading()
         self.validate_submission()
+
+    def _handle_submission_file_loading(self):
+        # Load the submission into a dict object if a file path is provided.
+        if os.path.isfile(self.submission_json_file_path):
+            try:
+                with open(
+                    self.submission_json_file_path, "r", encoding="utf-8"
+                ) as file:
+                    self.submission_dict = json.load(file)
+                print("Data successfully loaded for validation.")
+                print(self.submission_dict)
+            except FileNotFoundError:
+                print(
+                    f"Error: The file '{self.submission_json_file_path}' "
+                    "was not found."
+                )
+            except json.JSONDecodeError:
+                print(
+                    f"Error: Failed to decode JSON from the file: "
+                    f"`{self.submission_json_file_path}`."
+                )
 
     def validate_datetime(self, date_string):
         """
@@ -49,27 +72,6 @@ class TugboatValidator:
                     return False
         return True
 
-    def _handle_submission_file_loading(self):
-        # Load the submission into a dict object if a file path is provided.
-        if os.path.isfile(self.submission_json_file_path):
-            try:
-                with open(
-                    self.submission_json_file_path, "r", encoding="utf-8"
-                ) as file:
-                    self.submission_dict = json.load(file)
-                print("Data successfully loaded for validation.")
-                print(self.submission_dict)
-            except FileNotFoundError:
-                print(
-                    f"Error: The file '{self.submission_json_file_path}' "
-                    "was not found."
-                )
-            except json.JSONDecodeError:
-                print(
-                    f"Error: Failed to decode JSON from the file: "
-                    f"`{self.submission_json_file_path}`."
-                )
-
     def _validate_type(self):
         """Validates the types of fields in the submission."""
         if "wcsd" not in self.submission_dict["type"].lower():
@@ -77,6 +79,34 @@ class TugboatValidator:
                 "Error: 'type' field must contain 'wcsd' for active "
                 "acoustics data."
             )
+
+    def validate_sea_area(self):
+        if self.submission_dict.get("seaArea", "") == "":
+            print("Field `seaArea` cannot be empty.", file=sys.stderr)
+
+    def validate_sources(self):
+        if self.submission_dict.get("sources", []) == []:
+            print("Field `sources` cannot be empty.", file=sys.stderr)
+
+    def validate_arrival_port(self):
+        if self.submission_dict.get("arrivalPort", "") == "":
+            print("Field `arrivalPort` cannot be empty.", file=sys.stderr)
+
+    def validate_departure_port(self):
+        if self.submission_dict.get("departurePort", "") == "":
+            print("Field `departurePort` cannot be empty.", file=sys.stderr)
+
+    def validate_instruments(self):
+        """This function validates all instruments and their attributes."""
+
+        for instrument in self.submission_dict.get("instruments", {}):
+            if instrument.get("releaseDate", "") == "":
+                print("Field `releaseDate` cannot be empty.", file=sys.stderr)
+            if instrument.get("status", "") == "":
+                print("Field `status` cannot be empty.", file=sys.stderr)
+            if instrument.get("calibrationDate", "") == "":
+                print("Field `calibrationDate` cannot be empty.",
+                      file=sys.stderr)
 
     def validate_submission(self):
         """Validates a submission."""
