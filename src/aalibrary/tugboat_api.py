e48c201a7a5fe3ad485e07e06b5f3d6d28f87f80
Hannan Khan <hannan.khan@tspi.net>
2026-02-02 12:41:13 -0600

    add: major updates to Tugboat API code

--- src/aalibrary/tugboat_api.py
+++ src/aalibrary/tugboat_api.py
@@ -8,14 +8,14 @@ import requests
 from dotenv import load_dotenv
 
 # For pytests-sake
-if __package__ is None or __package__ == "":
-    # uses current directory visibility
-    from utils import cloud_utils
-    from utils.tugboat_validations import TugboatValidator
-else:
-    # uses current package visibility
-    from aalibrary.utils import cloud_utils
-    from aalibrary.utils.tugboat_validations import TugboatValidator
+# if __package__ is None or __package__ == "":
+#     # uses current directory visibility
+#     from utils import cloud_utils
+#     from utils.tugboat_validations import TugboatValidator
+# else:
+#     # uses current package visibility
+#     from aalibrary.utils import cloud_utils
+#     from aalibrary.utils.tugboat_validations import TugboatValidator
 
 
 class TugboatAPI:
@@ -52,17 +52,17 @@ class TugboatAPI:
         """Creates vars for use later."""
 
         # Create google connection objects to download the Tugboat creds
-        if (
-            ("gcp_stor_client" not in self.__dict__)
-            or ("gcp_bucket_name" not in self.__dict__)
-            or ("gcp_bucket" not in self.__dict__)
-        ):
-            self.gcp_stor_client, self.gcp_bucket_name, self.gcp_bucket = (
-                cloud_utils.setup_gcp_storage_objs(
-                    project_id=self.project_id,
-                    gcp_bucket_name=self.gcp_bucket_name,
-                )
-            )
+        # if (
+        #     ("gcp_stor_client" not in self.__dict__)
+        #     or ("gcp_bucket_name" not in self.__dict__)
+        #     or ("gcp_bucket" not in self.__dict__)
+        # ):
+        #     self.gcp_stor_client, self.gcp_bucket_name, self.gcp_bucket = (
+        #         cloud_utils.setup_gcp_storage_objs(
+        #             project_id=self.project_id,
+        #             gcp_bucket_name=self.gcp_bucket_name,
+        #         )
+        #     )
 
     def _set_tugboat_credentials(self):
         """Sets the tugboat credentials for AALibrary from the google storage
@@ -76,12 +76,13 @@ class TugboatAPI:
                 # Load from GCP bucket
                 # Download as string so that we dont have to worry about
                 # storing it anywhere
-                os.environ["TUGBOAT_CREDENTIALS"] = (
-                    cloud_utils.download_file_from_gcp_as_string(
-                        gcp_bucket=self.gcp_bucket,
-                        blob_file_path="other/tugboat_creds",
-                    )
-                )
+                # os.environ["TUGBOAT_CREDENTIALS"] = (
+                #     cloud_utils.download_file_from_gcp_as_string(
+                #         gcp_bucket=self.gcp_bucket,
+                #         blob_file_path="other/tugboat_creds",
+                #     )
+                # )
+                ...
             # Set the credentials
             self.__tugboat_cred = os.environ.get("TUGBOAT_CREDENTIALS")
         self.headers["Authorization"] = f"Bearer {self.__tugboat_cred}"
@@ -100,6 +101,7 @@ class TugboatAPI:
         except requests.exceptions.RequestException as e:
             print(f"Error during GET request: {e}")
 
+    ############################################################### SUBMISSIONS
     def create_empty_submission_file(
         self, file_download_directory: str = ".", file_name: str = ""
     ):
@@ -151,21 +153,11 @@ class TugboatAPI:
                 submission details. Defaults to None.
         """
 
-        validator = TugboatValidator(
-            submission_json_file_path=submission_json_file_path,
-            submission_dict=submission_dict,
-        )
-        validator.validate_submission()
-
-    def get_all_platforms(self):
-        """Fetches all platforms from the Tugboat API."""
-        url = urllib.parse.urljoin(self.tugboat_api_url, "platforms")
-        return self._get_request_as_json(url)
-
-    def get_all_instruments(self):
-        """Fetches all instruments from the Tugboat API."""
-        url = urllib.parse.urljoin(self.tugboat_api_url, "instruments")
-        return self._get_request_as_json(url)
+        # validator = TugboatValidator(
+        #     submission_json_file_path=submission_json_file_path,
+        #     submission_dict=submission_dict,
+        # )
+        # validator.validate_submission()
 
     def post_new_submission(self, submission_json_file_path: str = ""):
         """Posts a new submission to the Tugboat API."""
@@ -219,26 +211,6 @@ class TugboatAPI:
             )
             print(response.text)
 
-    def post_new_person(self, person_json: dict):
-        """Posts a new person to the Tugboat API."""
-
-        # Create the URL for the person endpoint
-        url = urllib.parse.urljoin(self.tugboat_api_url, "persons")
-
-        response = requests.post(
-            url, headers=self.headers, json=person_json, timeout=10
-        )
-        # Checking the response status code
-        if response.status_code == 201:  # 201 Created for successful POST
-            print("POST request successful!")
-            print("Response JSON:")
-            print(response.json())
-        else:
-            print(
-                f"POST request failed with status code: {response.status_code}"
-            )
-            print(response.text)
-
     def get_all_submissions(self):
         """Fetches all submissions from the Tugboat API."""
         url = urllib.parse.urljoin(self.tugboat_api_url, "submissions")
@@ -252,6 +224,15 @@ class TugboatAPI:
         )
         return self._get_request_as_json(url)
 
+    def get_submission_job_by_id(self, submission_id: str):
+        """Fetches the job associated with a submission by its ID from the
+        Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url, f"submissions/{submission_id}/job"
+        )
+        return self._get_request_as_json(url)
+
+    ###################################################################### JOBS
     def get_all_jobs(self):
         """Fetches all jobs from the Tugboat API. Useful for checking
         the status of a submission."""
@@ -267,25 +248,121 @@ class TugboatAPI:
         url = urllib.parse.urljoin(self.tugboat_api_url, f"jobs/{job_id}")
         return self._get_request_as_json(url)
 
+    #################################################################### PEOPLE
     def get_all_people(self) -> list:
         """Fetches all people & their info from the Tugboat API."""
 
-        url = urllib.parse.urljoin(
-            self.tugboat_api_url, "people?itemsPerPage=1073741824"
-        )
+        url = urllib.parse.urljoin(self.tugboat_api_url, "people")
         return self._get_request_as_json(url)["items"]
+
+    def get_person_by_id(self, person_id: str) -> dict:
+        """Fetches a person by their ID from the Tugboat API."""
+        url = urllib.parse.urljoin(self.tugboat_api_url, f"people/{person_id}")
+        return self._get_request_as_json(url)
+
+    def update_person_by_id(self, person_id: str, person_json: dict) -> dict:
+        """Updates a person by their ID in the Tugboat API."""
+        # TODO: Implement function to update person by ID in the Tugboat API.
 
     def search_people_by_email(self, email: str) -> dict:
         """Searches for people by email in the Tugboat API."""
         url = urllib.parse.urljoin(
             self.tugboat_api_url,
-            f"people?itemsPerPage=1073741824&email={urllib.parse.quote(email)}",
+            f"people?email={urllib.parse.quote(email)}",
         )
         resp = self._get_request_as_json(url)
         if resp["totalItems"] == 0:
             return None
         else:
             return resp["items"]
+
+    def post_new_person(self, person_json: dict):
+        """Posts a new person to the Tugboat API."""
+
+        # Create the URL for the person endpoint
+        url = urllib.parse.urljoin(self.tugboat_api_url, "people")
+
+        response = requests.post(
+            url, headers=self.headers, json=person_json, timeout=10
+        )
+        # Checking the response status code
+        if response.status_code == 201:  # 201 Created for successful POST
+            print("POST request successful!")
+            print("Response JSON:")
+            print(response.json())
+        else:
+            print(
+                f"POST request failed with status code: {response.status_code}"
+            )
+            print(response.text)
+
+    ############################################################# ORGANIZATIONS
+    def get_organization_by_id(self, organization_id: str) -> dict:
+        """Fetches an organization by its ID from the Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url, f"organizations/{organization_id}"
+        )
+        return self._get_request_as_json(url)
+
+    def get_all_organizations(self) -> list:
+        """Fetches all organizations from the Tugboat API."""
+
+        url = urllib.parse.urljoin(self.tugboat_api_url, "organizations")
+        return self._get_request_as_json(url)["items"]
+
+    ################################################################# PLATFORMS
+    def get_platform_by_id(self, platform_id: str) -> dict:
+        """Fetches a platform by its ID from the Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url, f"platforms/{platform_id}"
+        )
+        return self._get_request_as_json(url)
+
+    def get_all_platforms(self) -> list:
+        """Fetches all platforms from the Tugboat API."""
+
+        url = urllib.parse.urljoin(self.tugboat_api_url, "platforms")
+        return self._get_request_as_json(url)["items"]
+
+    ############################################################### INSTRUMENTS
+    def get_instrument_by_id(self, instrument_id: str) -> dict:
+        """Fetches an instrument by its ID from the Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url, f"instruments/{instrument_id}"
+        )
+        return self._get_request_as_json(url)
+
+    def search_instruments_by_short_name(self, short_name: str) -> dict:
+        """Searches for instruments by short name in the Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url,
+            f"instruments?shortName={urllib.parse.quote(short_name)}",
+        )
+        resp = self._get_request_as_json(url)
+        if resp["totalItems"] == 0:
+            return None
+        else:
+            return resp["items"]
+
+    def get_all_instruments(self) -> list:
+        """Fetches all instruments from the Tugboat API."""
+
+        url = urllib.parse.urljoin(self.tugboat_api_url, "instruments")
+        return self._get_request_as_json(url)["items"]
+
+    ################################################################# SEA AREAS
+    def get_sea_area_by_id(self, sea_area_id: str) -> dict:
+        """Fetches a sea area by its ID from the Tugboat API."""
+        url = urllib.parse.urljoin(
+            self.tugboat_api_url, f"sea-areas/{sea_area_id}"
+        )
+        return self._get_request_as_json(url)
+
+    def get_all_sea_areas(self) -> list:
+        """Fetches all sea areas from the Tugboat API."""
+
+        url = urllib.parse.urljoin(self.tugboat_api_url, "sea-areas")
+        return self._get_request_as_json(url)["items"]
 
 
 if __name__ == "__main__":
@@ -294,11 +371,21 @@ if __name__ == "__main__":
     #     file_download_directory=".",
     #     file_name="tugboat_test_submission.json",
     # )
-    print(tb_api.get_all_platforms())
-    print(tb_api.get_all_instruments())
+    # print(tb_api.get_all_organizations())
+    # print(tb_api.get_organization_by_id("151"))
+
+    # print(tb_api.get_all_platforms())
+    # print(tb_api.get_platform_by_id("51"))
+    # print(tb_api.get_all_platforms())
+    # print(tb_api.get_all_instruments())
+    # print(tb_api.get_instrument_by_id("301"))
+    # print(tb_api.search_instruments_by_short_name("EM3002"))
+    # print(tb_api.get_all_sea_areas())
+    # print(tb_api.get_sea_area_by_id("51"))
     # tb_api.post_new_submission(
     #     submission_json_file_path="./tugboat_test_submission.json"
     # )
     # tb_api.get_submission_by_id(submission_id="test0")
     # print(tb_api._tugboat_cred)
-    tb_api.get_all_submissions()
+    print(tb_api.get_all_submissions())
+    print(tb_api.get_submission_job_by_id(submission_id="156"))
