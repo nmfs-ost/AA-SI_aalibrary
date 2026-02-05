8512eefe23204f5bd3310eab88709762fc0f8029
Hannan Khan <hannan.khan@tspi.net>
2026-02-05 11:43:34 -0600

    add: func to disable cloud sdk warnings

--- src/aalibrary/__init__.py
+++ src/aalibrary/__init__.py
@@ -1,6 +1,7 @@
 # set the correct logging here
 import sys
 import logging
+import warnings
 
 # Import sub-packages
 # flake8: noqa
@@ -22,3 +23,11 @@ formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 console.setFormatter(formatter)
 # add the handler to the root logger
 logging.getLogger("").addHandler(console)
+
+def _disable_cloud_sdk_warning():
+    """Disable the warning about missing Cloud SDK credentials."""
+
+    warnings.filterwarnings(
+        "ignore",
+        message="Your application has authenticated using end user credentials",
+    )
