19a7b3a73c19eef9be6a05ed750352d414eb6cb5
Hannan Khan <hannan.khan@tspi.net>
2026-02-20 17:39:22 -0600

    add: sql formatter, functions to parse and execute, correct unioninzation of sql query, and more formatting of parentheses in sql query

--- src/aalibrary/utils/multi_fetch_yaml_parser.py
+++ src/aalibrary/utils/multi_fetch_yaml_parser.py
@@ -4,7 +4,22 @@ This file/object will parse that yaml string/file into SQL query that can be
 executed on the metadata DB."""
 
 from pprint import pprint
+from typing import List
 import yaml
+import sqlparse
+
+from google.cloud import bigquery
+
+# For pytests-sake
+if __package__ is None or __package__ == "":
+    # uses current directory visibility
+    from cloud_utils import bq_query_to_pandas
+    from helpers import normalize_ship_name
+else:
+    from aalibrary.utils.cloud_utils import bq_query_to_pandas
+    from aalibrary.utils.helpers import (
+        normalize_ship_name,
+    )
 
 
 class YAMLParser:
@@ -18,13 +33,21 @@ class YAMLParser:
             metadata DB, or as a list of s3 object keys that all match the
             parameters given in the requests."""
 
-    def __init__(self, yaml_file_path: str = "", yaml_dict: dict = None):
+    def __init__(
+        self,
+        yaml_file_path: str = "",
+        yaml_dict: dict = None,
+        gcp_project_id: str = "ggn-nmfs-aa-dev-1",
+    ):
         self.yaml_file_path = yaml_file_path
         self.yaml_dict = yaml_dict
+        self.gcp_project_id = gcp_project_id
         self.requests = []
+        self.sql_query = ""
         # Load the yaml object, or read the file into a yaml object.
         self._handle_file_loading()
         self._parse_requests()
+        self._generate_sql_query()
 
     def _handle_file_loading(self):
         if self.yaml_file_path != "":
@@ -41,7 +64,22 @@ class YAMLParser:
 
     def _parse_requests(self):
         for request in self.yaml_dict["requests"]:
-            self.requests.append(RequestParser(request_dict=request))
+            self.requests.append(
+                RequestParser(
+                    request_dict=request, gcp_project_id=self.gcp_project_id
+                )
+            )
+
+    def _generate_sql_query(self):
+        for idx, request in enumerate(self.requests):
+            if idx == 0:
+                self.sql_query += f"""(\n{request.sql_query}\n)"""
+            else:
+                self.sql_query += f"""\nUNION ALL\n(\n{request.sql_query}\n)"""
+        # Format the SQL query for readability
+        self.sql_query = sqlparse.format(
+            self.sql_query, reindent=True, keyword_case="upper"
+        )
 
 
 class RequestParser:
@@ -51,10 +89,20 @@ class RequestParser:
     NOTE: You can view the individual SQL conditional clause for each request
     by using print(request.sql_conditions_clause)"""
 
-    def __init__(self, request_dict: dict = None):
+    def __init__(
+        self,
+        request_dict: dict = None,
+        gcp_project_id: str = "ggn-nmfs-aa-dev-1",
+    ):
         self.request_dict = request_dict
+        self.gcp_project_id = gcp_project_id
+        self.sql_query = (
+            f"""SELECT *\nFROM `{self.gcp_project_id}.metadata.ncei_cache`\n"""
+        )
         self.sql_conditions_clause = """WHERE\n"""
+
         self._create_sql_conditions_clause()
+        self._create_sql_query()
 
     def _create_sql_conditions_clause(self):
         self._parse_vessel_conditions()
@@ -64,8 +112,11 @@ class RequestParser:
 
     def _parse_vessel_conditions(self):
         if "vessel" in self.request_dict:
+            ship_name_normalized = normalize_ship_name(
+                self.request_dict["vessel"]
+            )
             self.sql_conditions_clause += (
-                f"""ship_name = '{self.request_dict["vessel"]}'\n"""
+                f"""ship_name_normalized = '{ship_name_normalized}'\n"""
             )
 
     def _parse_survey_conditions(self):
@@ -109,7 +160,7 @@ class RequestParser:
 
     def _parse_time_window_conditions(self):
         if "time-windows" in self.request_dict:
-            self.sql_conditions_clause += """\nAND\n"""
+            self.sql_conditions_clause += """\nAND\n(\n"""
             if isinstance(self.request_dict["time-windows"], list):
                 for idx, time_dict in enumerate(
                     self.request_dict["time-windows"]
@@ -119,7 +170,7 @@ class RequestParser:
                             f"""(file_datetime >= '{time_dict["start"]}'\n"""
                         )
                         self.sql_conditions_clause += (
-                            f"""file_datetime <= '{time_dict["end"]}')\n"""
+                            f"""AND file_datetime <= '{time_dict["end"]}')\n"""
                         )
                     else:
                         self.sql_conditions_clause += (
@@ -127,13 +178,51 @@ class RequestParser:
                             f""" >= '{time_dict["start"]}'\n"""
                         )
                         self.sql_conditions_clause += (
-                            f"""file_datetime <= '{time_dict["end"]}')\n"""
+                            f"""AND file_datetime <= '{time_dict["end"]}')\n"""
                         )
+            self.sql_conditions_clause += """)\n"""
+
+    def _create_sql_query(self):
+        self.sql_query += f"""\n{self.sql_conditions_clause}\n"""
+
+
+def parse_yaml_file(yaml_file_path: str) -> str:
+    """Parses a YAML file and returns a sql query string that can be executed
+    on the metadata DB to return the results of the YAML submission."""
+    yaml_parsed = YAMLParser(yaml_file_path=yaml_file_path)
+    return yaml_parsed.sql_query
+
+
+def execute_sql_query(
+    sql_query: str, client: bigquery.Client = None
+) -> List[str]:
+    """This function executes the sql query generated from the YAML parsing.
+    The results of this query will be the object keys of the files in the
+    metadata DB that match the parameters given in the YAML."""
+    df = bq_query_to_pandas(query=sql_query, client=client)
+    return df["s3_object_key"].tolist()
+
+
+def parse_yaml_and_fetch_results(
+    yaml_file_path: str, client: bigquery.Client = None
+) -> List[str]:
+    """This function combines the two steps of parsing the YAML and executing
+    the resulting SQL query to return the results of the YAML submission."""
+    sql_query = parse_yaml_file(yaml_file_path=yaml_file_path)
+    results = execute_sql_query(sql_query=sql_query, client=client)
+    return results
 
 
 if __name__ == "__main__":
     yaml_test = YAMLParser(
         yaml_file_path=r"C:\Users\Hannah Khan\Desktop\repos\AA-SI_aalibrary\other\scripts\multi-fetch-algo-template.yaml"
     )
-    yaml_test._print_yaml_dict()
-    yaml_test._print_requests_sql()
+    # yaml_test._print_yaml_dict()
+    # yaml_test._print_requests_sql()
+    print(yaml_test.sql_query)
+    # results = parse_yaml_and_fetch_results(
+    #     yaml_file_path=r"C:\Users\Hannah Khan\Desktop\repos\AA-SI_aalibrary\other\scripts\multi-fetch-algo-template.yaml",
+    #     client=bigquery.Client(project="ggn-nmfs-aa-dev-1"),
+    # )
+    # print(results)
+    # print(len(results))
